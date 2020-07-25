e3f1b59602ed9d905ed58339bd15bf1f175aa947
Amit Kadam <amit.bk03@gmail.com>
2020-07-25 16:29:30 +0530

    checkout route and view created

---
+++ resources/views/order/checkout.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="card">
+                <div class="card-header">Checkout</div>
+
+                <div class="card-body">
+                    
+                    <form action="checkout" method="POST">
+
+                        <div class="form-group">
+                            <label for="amount">Amount:</label>
+                            <input type="text" class="form-control" id="amount" name="amount" value="0">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Checkout</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
